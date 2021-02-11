993b6f8833dbbcdc259f31ade7c368b67cd793a3
Gal Agai <gal@nbv.co.il>
2021-02-11 16:37:01 +0200

    Fin

---
+++ script.js
@@ -0,0 +1,176 @@
+// Get Elements
+const $keyboard = document.getElementById("keyboard")
+const $header = document.getElementById("top")
+const $language = document.getElementById("language")
+const $audioTag = document.getElementById("audio")
+const $container = document.querySelector(".container")
+const $startBTN = document.querySelector(".start-game")
+// Constants
+const letters = [
+  "א",
+  "ב",
+  "ג",
+  "ד",
+  "ה",
+  "ו",
+  "ז",
+  "ח",
+  "ט",
+  "י",
+  "כ",
+  "ל",
+  "מ",
+  "נ",
+  "ס",
+  "ע",
+  "פ",
+  "צ",
+  "ק",
+  "ר",
+  "ש",
+  "ת",
+]
+
+const sounds = {
+  wrong: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/250758/wronganswer.mp3",
+  correct:
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/250758/rightanswer.mp3",
+  he: {
+    where: "./sounds/where.ogg",
+    א: "./sounds/alef.ogg",
+    ב: "./sounds/bet.ogg",
+    ג: "./sounds/gimel.ogg",
+    ד: "./sounds/daled.ogg",
+    ה: "./sounds/hey.ogg",
+    ו: "./sounds/vav.ogg",
+    ז: "./sounds/zain.ogg",
+    ח: "./sounds/chet.ogg",
+    ט: "./sounds/tet.ogg",
+    י: "./sounds/yud.ogg",
+    כ: "./sounds/kaf.ogg",
+    ל: "./sounds/lamed.ogg",
+    מ: "./sounds/mem.ogg",
+    נ: "./sounds/noon.ogg",
+    ס: "./sounds/samech.ogg",
+    ע: "./sounds/ayin.ogg",
+    פ: "./sounds/pay.ogg",
+    צ: "./sounds/tzadi.ogg",
+    ק: "./sounds/kuf.ogg",
+    ר: "./sounds/resh.ogg",
+    ש: "./sounds/shin.ogg",
+    ת: "./sounds/taf.ogg",
+  },
+}
+// Play selected sounds
+const playSound = (selectedLang, sound) => {
+  console.log(sound)
+  $audioTag.src = sounds[selectedLang][sound]
+  $audioTag.play()
+}
+const playSounds = (number) => {
+  console.log($language.value)
+  playSound($language.value, "where")
+
+  setTimeout(() => {
+    playSound($language.value, number)
+  }, 1750)
+}
+const startTheGame = ($event) => {
+  const isStartButton = $event.target.localName === "button"
+  if (isStartButton) {
+    const conrrectAnswer = $keyboard.dataset.answer
+    return playSounds(conrrectAnswer)
+  }
+}
+
+const selectAnswer = ($event) => {
+  const isLiElement = $event.target.localName === "li"
+  const isReplay = $event.target.dataset.id === "replay"
+  if (!isLiElement) {
+    return false
+  }
+
+  const currentSelectedAnswer = $event.target.dataset.id
+  const correctAnswer = $keyboard.dataset.answer
+
+  if (isReplay) {
+    return playSounds(correctAnswer)
+  }
+  console.log("S", currentSelectedAnswer, "C", correctAnswer)
+
+  if (currentSelectedAnswer === correctAnswer) {
+    $container.classList.add("green-background")
+
+    $audioTag.src = sounds.correct
+    $audioTag.play()
+
+    setTimeout(() => {
+      $container.classList.remove("green-background")
+      createBoard()
+    }, 1000)
+  } else {
+    $container.classList.add("red-background")
+
+    $audioTag.src = sounds.wrong
+    $audioTag.play()
+
+    setTimeout(() => {
+      playSounds(correctAnswer)
+      $container.classList.remove("red-background")
+    }, 1300)
+  }
+}
+// Shuffle numbers
+const shuffle = (numberArray) => {
+  let counter = numberArray.length
+  while (counter > 0) {
+    let index = Math.floor(Math.random() * counter)
+    counter--
+    let temp = numberArray[counter]
+    numberArray[counter] = numberArray[index]
+    numberArray[index] = temp
+  }
+  return numberArray
+}
+
+// Create Board
+const createBoard = () => {
+  $keyboard.innerHTML = ""
+  $header.innerHTML = ""
+  const random = Math.floor(Math.random() * 22)
+  // console.log(random)
+  $keyboard.dataset.answer = letters[random]
+  // $keyboard.dataset.numAnswer = random
+
+  playSounds(letters[random])
+  const randomLetter = shuffle(letters)
+  randomLetter.forEach((letter) => {
+    const liElement = document.createElement("li")
+    liElement.innerText = letter
+    liElement.dataset.id = letter
+    $keyboard.appendChild(liElement)
+  })
+  const rePlay = document.createElement("li")
+  rePlay.classList.add("replay")
+  rePlay.dataset.id = "replay"
+  rePlay.innerHTML = "שמע שוב את השאלה"
+  $keyboard.appendChild(rePlay)
+
+  const playButton = document.createElement("BUTTON")
+  playButton.classList.add("start-game")
+  playButton.dataset.id = "start-game"
+  playButton.innerHTML = "התחל משחק"
+  $header.appendChild(playButton)
+}
+createBoard()
+
+// Create audio element
+const audioTag = document.createElement("audio")
+audioTag.type = "audio/ogg"
+audioTag.id = "audio"
+document.body.appendChild(audioTag)
+const audio = document.createElement("audio")
+
+// Event listeners
+$keyboard.addEventListener("click", selectAnswer)
+$header.addEventListener("click", startTheGame)
